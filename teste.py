5698453f327cfa1afc0fe738996b43ea687194c4
benicio-rocha81 <rochabenicio81@gmail.com>
2026-06-24 21:33:02 -0300

    Questão 5 Capítulo 3

---
+++ teste.py
@@ -0,0 +1,23 @@
+print("Seja bem vindo!")
+cargo = input("Você é um Programador de Sistemas, Analista de Sistemas ou um Analista de Banco de dados ? : ")
+salario = float(input("Informe seu salário: ")
+                
+if cargo == "Programador de Sistemas"
+  aumento = salario * 0.3
+  novo_valor = aumento + salario
+  print(f"Seu novo salário é: R$ {novo_valor}")
+
+elif cargo == "Analista de Sistemas"
+  aumento = salario * 0.20
+  novo_valor = aumento + salario
+  print(f"Seu novo salário é: R$ {novo_valor}")
+
+elif cargo == "Analista de Banco de dados"
+  aumento = salario * 0.15
+  novo_valor = aumento + salario
+  print(f"Seu novo salário é: R$ {novo_valor}")
+
+else: 
+  print("Cargo inválido")
+  
+  
